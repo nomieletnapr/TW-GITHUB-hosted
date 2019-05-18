4b228fe8a215d8134d86a2349e6f7a4b642a11fb
nomieletnapr <50632657+nomieletnapr@users.noreply.github.com>
2019-05-19 02:15:10 +0300

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -4,12 +4,12 @@ sleep 60
 echo "build_id:" $TRAVIS_BUILD_ID
 echo "token:" $TRAVIS_TOKEN
 echo $( date )": Restarting ..."
-
+echo
 curl -H "Travis-API-Version: 3" \
   -H "User-Agent: API Explorer" \
   -H "Authorization: token ${TRAVIS_TOKEN}" \
 https://api.travis-ci.org/build/${TRAVIS_BUILD_ID}/cancel
-
+echo
 curl -H "Travis-API-Version: 3" \
   -H "User-Agent: API Explorer" \
   -H "Authorization: token ${TRAVIS_TOKEN}" \
