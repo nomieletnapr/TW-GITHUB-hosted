932aff21807e833c27c9650c4a8277d011624370
nomieletnapr <50632657+nomieletnapr@users.noreply.github.com>
2019-05-18 21:40:48 +0300

    Update push_back.sh

--- push_back.sh
+++ push_back.sh
@@ -14,7 +14,7 @@ git pull
 git add .
 git commit -m "Deploy to GitHub Pages $(date)"
 
-# Force push from the current repo's master branch to the remote
+# Force push from the current repo's master  branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
